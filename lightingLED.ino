2c54277e377716b7ec7a014d65a27fa6a9c4fe75
luna01926 <echinochloa01926@gmail.com>
2016-11-10 00:46:21 +0900

    コンパイル通った

--- lightingLED.ino
+++ lightingLED.ino
@@ -1,17 +1,20 @@
-int LEDpin[14] = {-1,-1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+/*int LEDpin[14] = {-1,-1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 int codes[4] = {'1', '0', 'P', '0'};
 int code_i = 0;
 unsigned long time;
-int i = 3;
-int enablePin = 2;
-int minPinNum = 2;
+int i = 4;
+int enablePin = 4;
+int minPinNum = 4;
 int code = '1';
+unsigned int sensorData;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LEDpin[2], OUTPUT);
-  pinMode(LEDpin[3], OUTPUT);
+  pinMode(LEDpin[4], OUTPUT);
+  pinMode(LEDpin[5], OUTPUT);
+  pinMode(LEDpin[6], OUTPUT);
+  pinMode(LEDpin[7], OUTPUT);
   time = millis();
 }
 
@@ -34,9 +37,16 @@ void normal(){
   delay(1000);
   digitalWrite(LEDpin[i], LOW);
   i++;
-  if(i > 1+enablePin) i = 2;
+  if(i > 3+enablePin) i = 4;
   digitalWrite(LEDpin[i], HIGH);
   delay(1000);
+}
+
+void normal1(unsigned int data){
+}
+
+void combination(){
+  
 }
 
 void loop() {
@@ -52,9 +62,11 @@ void loop() {
     case 'P':
       normal();
       break;
+    case 'C':
+      combination();  
+      break;
   }
   code_i++;
   if(code_i > 3)code_i = 0;
 }
-
-
+*/
---
+++ testcode/testcode.ino
@@ -0,0 +1,89 @@
+const unsigned int data[] = {0x8096, 0xE064, 0x4000, 0xA04C, 0x0000};
+const int LEDpin[14] = {-1,-1, -1, -1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+unsigned int data_i = 0;
+
+/* Debug mode */
+bool isDebugMode = true;
+
+/* pulse data */
+bool pulse;
+int objectID;
+int mode;
+int bpm;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LEDpin[4], OUTPUT); // 
+  pinMode(LEDpin[5], OUTPUT); //
+  pinMode(LEDpin[6], OUTPUT); // 
+  pinMode(LEDpin[7], OUTPUT); //
+}
+
+void parseData(unsigned int d){
+  pulse = (0x8000&d) >> 15;
+  int mode1 = (0x4000&d) >> 14;
+  int mode2 = (0x2000&d) >> 13;
+  objectID = (0x1000&d) >> 12;
+  bpm = (0x00FF&d);
+  mode = 10*mode1 + mode2;
+  if(isDebugMode){
+    Serial.println("pulse : ");
+    Serial.println(pulse);
+    Serial.println("mode : ");
+    Serial.println(mode, DEC);
+    Serial.println("objectID : ");
+    Serial.println(objectID, DEC);
+    Serial.println("bpm : ");
+    Serial.println(bpm, DEC);
+  }
+}
+
+void all_off(){
+  for(int i = 4; i <= 7; ++i){
+    digitalWrite(i, LOW);
+  }
+}
+
+void dededen(int x = 7){
+  for(int j = 0; j < 2; ++j){
+    digitalWrite(4, HIGH);
+    delay(100);
+    for(int i = 5; i <= 7; i++){
+      digitalWrite(LEDpin[i], HIGH);
+      delay(100);
+      digitalWrite(LEDpin[i-1], LOW);      
+    }
+    delay(100);
+    digitalWrite(7, LOW);
+    delay(100);
+  }
+  for(int i = 4; i <= x; ++i){
+    digitalWrite(LEDpin[i], HIGH);
+    delay(100);
+  }
+  delay(1000);
+  all_off();
+}
+
+void combination(){
+  randomSeed(millis());
+  double diff = 0.5*1000;
+  int pulseDelay = random(0, 1000);
+  Serial.println("pulseDelay:");
+  Serial.println(pulseDelay);
+  if(pulseDelay < diff){
+    dededen();
+  }
+  else{
+    int rnd = random(4,7);
+    Serial.println("random:");
+    Serial.println(rnd);
+    dededen(rnd);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  combination();
+}
